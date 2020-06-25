62f9b0967fbeeac1b58541403cab5888147bb107
shipra111 <shipragupta012@yahoo.in>
2020-06-25 15:01:14 +0530

    Add method to add upto 2 numbers

--- Calculator/src/source/codeSample/StringCalculator.java
+++ Calculator/src/source/codeSample/StringCalculator.java
@@ -4,8 +4,14 @@ public class StringCalculator {
 	
 	public int Add(String numbers) {
 		int sum=0;
-		if(numbers != null && numbers != "") {
-			
+		if(numbers != null && !numbers.equals("")) {
+			if(numbers.contains(",")) {
+				int index = numbers.indexOf(",");				
+				sum=Integer.parseInt(numbers.substring(0,index))
+						+Integer.parseInt(numbers.substring(index+1));
+			}else {
+				sum=Integer.parseInt(numbers);
+			}
 		}
 		return sum;
 	}
--- Calculator/src/test/codeSample/StringCalculatorTest.java
+++ Calculator/src/test/codeSample/StringCalculatorTest.java
@@ -1,8 +1,10 @@
 package test.codeSample;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import source.codeSample.StringCalculator;
 
@@ -11,11 +13,19 @@ class StringCalculatorTest {
 	StringCalculator obj = new StringCalculator();
 	
 	@Test
-	void AddBlankStringCase() {
+	void test_AddBlankStringCase() {
 		int sum1 = obj.Add("");
 		int sum2 = obj.Add(null);
 		assertEquals(0,sum1);
 		assertEquals(0,sum2);
 	}
+	
+	@ParameterizedTest
+	@CsvSource(value={"0>0","1>1","1,2>3","11>11","23,4>27","45,400>445"},delimiter='>')
+	void test_AddOneOrTwoStringCase(String input,String expectedOutput) {
+		assertEquals(Integer.parseInt(expectedOutput),obj.Add(input));
+	}
+	
+	
 
 }
